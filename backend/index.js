30bcbee1515a9c45b0134a951d372875e7275231
SAEED <149547355+saeedsaiyed01@users.noreply.github.com>
2025-02-14 11:58:04 +0530

    .

--- backend/index.js
+++ backend/index.js
@@ -19,7 +19,9 @@ app.get("/", (req, res) => {
   
 // CORS configuration
 const allowedOrigins = [
-    'http://localhost:5173','http://localhost:5174' // Your frontend local origin
+    'http://localhost:5173','http://localhost:5174',
+    " https://frontend-v2-qk07wj39y-saeeds-projects-59535290.vercel.app",
+     // / // Your frontend local origin
     // Add more origins as needed
 ];
 
